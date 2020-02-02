7a235b1e956c5461aeaad75c85d463615f26e678
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-01 21:32:24 -0300

    refactor: generalizate home javascript

--- js/home.js
+++ js/home.js
@@ -1,8 +1,13 @@
-$('.novidades button').click(function() {
-    $('.novidades').toggleClass('painel-aberto');
-})
+// $('.novidades button').click(function() {
+//     $('.novidades').toggleClass('painel-aberto');
+// });
 
 
-$('.mais-vendidos button').click(function() {
-    $('.mais-vendidos').toggleClass('painel-aberto');
-})
+// $('.mais-vendidos button').click(function() {
+//     $('.mais-vendidos').toggleClass('painel-aberto');
+// });
+
+
+$('.painel button').click(function() {
+    $(this).parent().toggleClass('painel-aberto');
+});
