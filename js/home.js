fc1a3cb610216963c76bc5451b7698ce327b4363
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-01 21:30:02 -0300

    feat: add button show more & style

--- js/home.js
+++ js/home.js
@@ -1,3 +1,8 @@
 $('.novidades button').click(function() {
     $('.novidades').toggleClass('painel-aberto');
 })
+
+
+$('.mais-vendidos button').click(function() {
+    $('.mais-vendidos').toggleClass('painel-aberto');
+})
